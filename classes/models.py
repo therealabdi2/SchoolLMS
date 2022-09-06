6f79a5f4dce76f3922ad8c1bf58c7998f547ed0b
Abdurrehman Afridi <abdurrehman.afridi.5@gmail.com>
2022-09-06 12:30:05 +0500

    make teacher model ManytoMany with Class model

--- classes/models.py
+++ classes/models.py
@@ -5,7 +5,7 @@ from accounts.models import TeacherProfile
 
 
 class ClassGrade(models.Model):
-    teacher = models.OneToOneField(TeacherProfile, on_delete=models.CASCADE)
+    teacher = models.ManyToManyField(TeacherProfile)
     name = models.CharField(max_length=20)
     image = models.ImageField(upload_to='class_grades')
 
