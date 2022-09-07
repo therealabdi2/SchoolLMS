882156f2704d96ab529f8e440f66a4edad87a683
Abdurrehman Afridi <abdurrehman.afridi.5@gmail.com>
2022-09-07 15:50:00 +0500

    Update database, and requirements

--- classes/admin.py
+++ classes/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from classes.models import ClassGrade
+from classes.models import ClassGrade, Questions
 
 admin.site.register(ClassGrade)
+admin.site.register(Questions)
--- classes/models.py
+++ classes/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from slugify import slugify
 
 # Create your models here.
 from accounts.models import TeacherProfile
@@ -18,3 +19,11 @@ class Questions(models.Model):
     type = models.CharField(max_length=10)
     difficulty = models.CharField(max_length=10)
     question_text = models.TextField()
+    slug_text = models.CharField(max_length=100, null=True)
+
+    def save(self, *args, **kwargs):
+        self.slug_text = slugify(self.question_text, max_length=15, word_boundary=True)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.category
